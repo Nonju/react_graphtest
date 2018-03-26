f607a5d851eb181c83a2f861ee45d5159a767d22
Hannes <hannes.albinsson@briteback.com>
2018-03-26 11:28:50 +0200

    D3 bargraph example

--- app/src/App.js
+++ app/src/App.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import D3 from './D3/';
+
 class App extends Component {
+
   render() {
     return (
       <div className="App">
@@ -10,9 +13,12 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1 className="App-intro">
+          Implementations:
+        </h1>
+
+        <D3 />
+        
       </div>
     );
   }
